36b16c4bfb00b6b989ddb1ea453e0f4fc4f97670
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-09-26 21:40:20 +0200

    add: main + mainly just cli handler

--- main.py
+++ main.py
@@ -1,15 +1,51 @@
-import asyncio
-from playwright.async_api import async_playwright
+import os
+import sys
+import time
+from modules.helpers import check_dir
 from modules.banners import print_banner
+from modules.pw_microsoft_learn import ms_learn
 
 
-async def main():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
-        await page.goto("https://docs.microsoft.com/en-us/learn/modules/chain-azure-functions-data-using-bindings/2-explore-input-and-output-binding-types-portal-lesson")
-        element_handle = await page.query_selector("#unit-inner-section")
-        await element_handle.screenshot(path="G:\\projects\\python\\POC\\google-images\\modules\\example.png")
-        await browser.close()
+working_dir = str(os.path.dirname(os.path.abspath(__file__)))
 
-asyncio.run(main())
+required_folders = {
+    "export": "\\export"
+}
+
+def main():
+    export_dir = working_dir + required_folders["export"]
+    print_banner("single", "header-start")
+
+    if check_dir(export_dir):
+        print_banner("double", "header-start", "check-dir-pos")
+    else:
+        print_banner("double", "header-start", "check-dir-neg")
+
+    print_banner("double", "header-start", "user-options")
+
+    selected = input("Please enter one of the above options here: ")
+
+    if selected == "1":
+        module_url = input("Please enter the module url below:\n")
+
+        if module_url:
+            if ms_learn(module_url, export_dir):
+                print_banner("double", "header-start", "export-success")
+                time.sleep(5)
+                main()
+            else:
+                print_banner("double", "header-start", "export-fail")
+                time.sleep(5)
+                main()
+        else:
+            print_banner("double", "header-start", "url-invalid")
+            time.sleep(5)
+            main()
+    elif selected == "2":
+        print_banner("single", "header-stop")
+        sys.exit(0)
+    else:
+        pass
+
+if __name__ == "__main__":
+    main()
