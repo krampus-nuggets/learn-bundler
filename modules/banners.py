087d0f479a9f7ba5a38a772b72e1b9a76e3263ab
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-09-26 21:38:02 +0200

    add: banners + banner print func

--- modules/banners.py
+++ modules/banners.py
@@ -1,5 +1,6 @@
 from os import system
 
+
 banners = {
     "header-start": """
 ╦  ╔═╗╔═╗╦═╗╔╗╔   ╔╗ ╦ ╦╔╗╔╔╦╗╦  ╔═╗╦═╗
@@ -11,7 +12,18 @@ by: krampus-nuggets
 ╔╗ ╦ ╦╔═╗
 ╠╩╗╚╦╝║╣ 
 ╚═╝ ╩ ╚═╝
-    """
+    """,
+    "check-dir-pos": "Directory Check: Required directories found, continuing...",
+    "check-dir-neg": "Directory Check: Required directories not found, they will be created...",
+    "user-options": """
+All available options can be found below:
+
+1. Export a MS Learn module | Press 1
+2. Close program | Press 2
+    """,
+    "url-invalid": "ERROR: Invalid URL | Program Reset",
+    "export-success": "EXPORT: Successful, file saved to export directory | Program Reset",
+    "export-fail": "EXPORT: Failed, something went wrong... | Program Reset"
 }
 
 def print_banner(ban_type, *banner):
